b07c0fabc469d08d1f2ef42859e07563bde36a7d
GODhanQ <hg.min2018@gmail.com>
2025-09-25 18:43:31 +0900

    요구 사항 추가

--- Lecture06_HandlingInputs/move_my_character_with_key.py
+++ Lecture06_HandlingInputs/move_my_character_with_key.py
@@ -1,5 +1,11 @@
 from pico2d import *
 
+# §상하좌우 방향키이를 이용해서 캐릭터를 상하좌우로 이동.
+# §IDLE 애니메이션 있어야 함.
+# §배경 : TUK_GROUND.png 사용
+# §화면 경계면에 다다르면 더 이상 진행하지 않음.
+# §소스코드 이름은 move_my_character_with_key.py
+
 Window_width, Window_height = 1280, 1024
 open_canvas(Window_width, Window_height)
 
