303820bcf9a674b3a91bcaa6db5dcf8e94414028
GODhanQ <hg.min2018@gmail.com>
2025-09-25 18:49:09 +0900

    테스트, 정상적인 종료 방법으로 화면 닫기 기능 추가

--- Lecture06_HandlingInputs/move_my_character_with_key.py
+++ Lecture06_HandlingInputs/move_my_character_with_key.py
@@ -12,11 +12,27 @@ open_canvas(Window_width, Window_height)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+running = True
+
+def event_handler():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 while True:
     clear_canvas()
     tuk_ground.draw(Window_width // 2, Window_height // 2)
     character.draw(Window_width, Window_height)
     update_canvas()
+
+    event_handler()
+    if not running:
+        break
+
     pass
 
 close_canvas()
